43c1cbe52550581f9a6b0eeeaf4877fc18b7a22c
martin sarsale <martin@properati.com>
2017-01-04 16:09:37 -0300

    WIP

--- scripts/startup_generator.sh
+++ scripts/startup_generator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export MYSQL_PASSWORD=$(cat /mysql-root-pw.txt)
+export MYSQL_PASSWORD=$()
 envtpl < /tmp/templates/startup.sh.template > /tmp/startup.sh
 chmod +x /tmp/startup.sh
 /tmp/startup.sh
